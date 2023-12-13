31fde6f0914b1675389cacb8b9352381efa0b366
JustinLeeVance <justinleevance94@gmail.com>
2023-12-12 21:36:42 -0500

    yay

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun (argument1 = `roller-skate`) {
+    const string1 = `This Saturday, I want to ${argument1}!`
+    return string1;
+}
+
+
+const mondayWork = function (para2 = `go to the office`) {
+    const string2 = `This Monday, I will ${para2}.`;
+    return string2;
+}
+
+
+//created a function within the first function, and returned it when declaring it
+//That allowed me to return the new variable string3 - meaning that function has closure
+function wrapAdjective (para1 = `*`) {
+    return function accentPoint(para2 = `special`){
+        let string3 = `You are ${para1}${para2}${para1}!`;
+        return string3;
+    }
+}
